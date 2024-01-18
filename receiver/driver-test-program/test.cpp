f0af877044c0627d344714bbfabad113c563a60a
Michael Hinterberger <michael-hinterberger@aon.at>
2024-01-18 17:30:22 +0100

    add driver test program

---
+++ receiver/driver-test-program/test.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+int main() {
+	char data[8] = {0};
+	uint64_t data0, data1, data2, data3, data4, data5, data6, data7;
+	uint64_t result = 0;
+
+	while(1) {
+		std::ifstream file("/dev/receiver");
+		
+		if(!file.read(data, 8)) {
+			continue;
+		}
+		
+		data0 = data[0];
+		data1 = data[1];
+		data2 = data[2];
+		data3 = data[3];
+		data4 = data[4];
+		data5 = data[5];
+		data6 = data[6];
+		data7 = data[7];
+		result = (data7 << 7) | (data6 << 6) | (data5 << 5) | (data4 << 4) | (data3 << 3) | (data2 << 2) | (data1 << 1) | data0; 		
+		std::cout << "data:" << result << std::endl;	
+	}
+	return 0;
+}
